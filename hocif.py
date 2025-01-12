08545a3ff191a1c2ae322f4b5831a9da9e3a26f6
halong <halong30042009@gmail.com>
2025-01-12 14:50:12 +0700

    bai 19

---
+++ hocif.py
@@ -0,0 +1,14 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def timchanle(a,b,c): # 10,5,5
+    if a-b==c: #10-5==5 
+        print("a tru b bang c")
+    else:
+        print("a tru b ko bang c")
+
+
+a = int(input('Enter 1st number: ')) # 10
+b = int(input('Enter 2nd number: ')) # 5
+d = int(input('Enter 3nd number: ')) # 5
+
+print(timchanle(a,b,d)) # 10,5,5
