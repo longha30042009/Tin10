95a9b7cc5207dc270350dee5b3ad4c7952fb7bf9
halong <halong30042009@gmail.com>
2025-01-12 21:27:30 +0700

    bt tin 10

---
+++ hocif3.py
@@ -0,0 +1,68 @@
+#de: cho nhap so  nguyen duong tuong ung vs so  thang  cua nam neu trung thang se hien ra ten thang neu khong hien ra thi ko ton tai  thang nay
+def thangnam(a):
+    if a<=12 and a>=1 :
+        if a==1:
+            print("thang 1")
+        elif a==2: 
+            print("thang 2")
+        elif a==3:
+            print("thang 3")
+        elif a==4:
+            print("thang 4") 
+        elif a==5:
+            print("thang 5")
+        elif a==6:
+            print("thang 6") 
+        elif a==7: 
+            print("thang 7")
+        elif a==8:
+            print("thang 8")
+        elif a==9:
+            print("thang 9") 
+        elif a==10:
+            print("thang 10")
+        elif a==11:
+            print("thang 11") 
+        elif a==12:
+             print("thang 12") 
+
+    else:
+         print("ko co thang nao")
+
+
+a = int(input('thang   ')) 
+
+thangnam(a)
+
+
+#lan thu 1
+#a=7
+#if 7<=12 (T)
+   #if a==1:  
+   #    if a==1:
+            #print("thang 1") (F)
+    #elif a==2: 
+            #print("thang 2") (F)
+    #elif a==3:
+            #print("thang 3") (F)
+    #elif a==4:
+            #print("thang 4") (F)
+    #elif a==5:
+            #print("thang 5") (F)
+    #elif a==6:
+            #print("thang 6") (F)
+    #elif a==7: 
+            #print("thang 7") (T)
+         #=> a=7= thang 7  
+
+
+
+ # lan thu 2
+ # a=-3
+ # if-3<=12 and -3>=1 (T and F => F)
+ #   print(ko co thang nao)       
+
+
+
+
+
